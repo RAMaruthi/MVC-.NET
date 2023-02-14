39cc0fbd89f24cda9750a7edad30b51cbb40d124
RAMaruthi <120400441+RAMaruthi@users.noreply.github.com>
2023-02-14 08:56:35 +0530

    login files

---
+++ DataRepo/Iuser.cs
@@ -0,0 +1,16 @@
+﻿using longIn.DataCompoent;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace longIn.DataRepo
+{
+   public interface Iuser
+    {
+        CustomerTbl ValidateUser(string emailAddress, string password);
+        void RegisterCustomer(CustomerTbl customer);
+        void UpdateCustomer(CustomerTbl customer);
+    }
+}
---
+++ DataRepo/UserLogin.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using longIn.DataCompoent;
+
+namespace longIn.DataRepo
+{
+    public class UserLogin : Iuser
+    {
+        private bool isValidEmail(string emailAddress)
+        {
+            var context = new MyEntity();
+            var rec = context.CustomerTbls.SingleOrDefault(c => c.cstEmail == emailAddress);
+            return rec == null;
+        }
+        public void RegisterCustomer(CustomerTbl customer)
+        {
+            var context = new MyEntity();
+            if (isValidEmail(customer.cstEmail))
+            {
+                context.CustomerTbls.Add(customer);
+                context.SaveChanges();
+            }
+            else
+            {
+                throw new Exception("Customer is Alredy registered,please click on Forget Password");
+            }
+        }
+
+        public void UpdateCustomer(CustomerTbl customer)
+        {
+            
+        }
+
+        public CustomerTbl ValidateUser(string emailAddress, string password)
+        {
+            var context = new MyEntity();
+            var customer = context.CustomerTbls.SingleOrDefault(c => c.cstEmail == emailAddress && c.password == password);
+            if (customer==null)
+            {
+                throw new Exception("Login Failed");
+            }
+            return customer;
+        }
+    }
+}
