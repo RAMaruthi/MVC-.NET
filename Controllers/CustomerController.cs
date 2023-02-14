fcf225f400effd7a9ab3bc0a3aee834c191f7d76
RAMaruthi <120400441+RAMaruthi@users.noreply.github.com>
2023-02-14 08:55:56 +0530

    Login controllers

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,56 @@
+﻿using longIn.DataCompoent;
+using longIn.DataRepo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace longIn.Controllers
+{
+    [AllowAnonymous]
+    public class CustomerController : Controller
+    {
+        //GET: Customer
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(string email, string pwd)
+        {
+            var com = new UserLogin();
+            try
+            {
+                var cst = com.ValidateUser(email, pwd);
+                Session["CustomerUser"] = cst;
+                FormsAuthentication.SetAuthCookie(cst.cstEmail, false);
+                FormsAuthentication.RedirectFromLoginPage(cst.cstEmail, false);
+                return View();
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("LoginError", ex.Message);
+                return View();
+            }
+        }
+        public ActionResult Add()
+        {
+            return View(new CustomerTbl());
+        }
+        [HttpPost]
+        public ActionResult Add(CustomerTbl customer)
+        {
+            var repo = new UserLogin();
+            repo.RegisterCustomer(customer);
+            return RedirectToAction("Login");
+        }
+
+        
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using longIn.DataCompoent;
+using longIn.DataRepo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace longIn.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public string Index()
+        {
+            if (Session["CustomerUser"] != null)
+            {
+                var cstDetails = Session["CustomerUser"] as CustomerTbl ;
+                return $"<h1>HomePage</h1></hr><p>Welcome come to  User {cstDetails.cstName}";
+            }
+            else
+            {
+                return $"<h1>HomePage</h1></hr><p>Welcome";
+            }
+        }
+
+
+    }
+}
