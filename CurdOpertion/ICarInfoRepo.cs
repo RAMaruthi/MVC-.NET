587fe9b1eeeac098677d9acfbe66b75846ac264b
RAMaruthi <120400441+RAMaruthi@users.noreply.github.com>
2023-02-14 08:54:01 +0530

    Curds Opertion

---
+++ CurdOpertion/ICarInfoRepo.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace WebAppMVC.CurdOpertion
+{
+    public interface ICarInfoRepo
+    {
+        void AddNewCar(CarInfo info);
+        void DeleteCar(int entryId);
+        CarInfo FindCar(int id);
+        List<CarInfo> GetAllCars();
+        void UpdateCar(CarInfo carInfo);
+    }
+}
